7f7e8f1d0651701b4074b6c0b77abfc1eef01268
galyverasi <galyverasi@gmail.com>
2021-10-25 11:22:17 -0600

    objects in canvas

--- readme.md
+++ readme.md
@@ -1,17 +1,24 @@
-# A Simple Game of Pong
+# A Simple Game of Ping Pong
 
 ## Wireframe
-(/pong.png)
+![pong](pong.png)
 
 ## Tech Stack
-* Html
+* HTML
 * CSS
 * Vanilla JavaScript
 
 ## MVP Goals
-* Display a pong board in browser
-* Functional control that will enable players to go up or down
+* Create the game using canvas
+* Display a ping pong board in the browser
+* Display two rectangles as the paddle
+* Display a ball that moves 45°
+* Implement collision detection and have the ball bounce if it hits the paddle
+* Implement collision detection and have the ball bounce if it top and bottom boundaries
+* Implement collision detection if ball hits the left and right boundaries
 * Enable two players
+* Left paddle will use keys "w" to go up "s" to go down
+* Right paddle will use arrow up and arrow down
 * Ball speed increases
 
 ## Stretch Goals
--- script.js
+++ script.js
@@ -1,54 +1,39 @@
 // make a simple game of pong
 // three elements in the canvas: two paddles and a ball
-// player 2 will use arrow up and arrow down
 
 const game = document.getElementById("canvas")
-
-game.setAttribute("width", getComputedStyle(game)["width"])
-game.setAttribute("height", getComputedStyle(game)["height"])
+    canvas.height = 500
+    canvas.width = 700
 
 const ctx = game.getContext("2d")
 
-// constructor function
-function Pong(x, y, color, width, height) {
+// constructor function for paddle
+function Paddle(x, y, color, width, height) {
     this.x = x
     this.y = y
     this.color = color
-    this.width = width
-    this.height = height
-    this.alive = true
+    this.height = 90
+    this.width = 15
     this.render = function () {
         ctx.fillStyle = this.color
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 }
 
-// render paddles and ball in the canvas
-let leftPaddle = new Pong(0, 0, "green", 15, 50)
-let rightPaddle = new Pong(805, 450, "red", 15, 50)
-let ball = new Pong(100, 100, "white", 10, 10 )
+// render left paddle in the canvas
+let leftPaddle = new Paddle(5, canvas.height / 2 - 45, "white", 15, 90)
 console.log("this is the left paddle", leftPaddle)
+// render right padde in the canvas
+let rightPaddle = new Paddle(680, canvas.height / 2 - 45, "white", 15, 90)
 console.log("this is the right paddle", rightPaddle)
-console.log("this is the ball", ball)
-
-// set up gameLoop function
-const gameLoop = () => {
-    // clear the canvas
-    ctx.clearRect(0, 0, game.width, game.height)
-    // render the paddles
-    if (leftPaddle.alive) {
-        leftPaddle.render()
-    }
-    rightPaddle.render()
-}
 
 // set up keycode functionality for left paddle
 // left paddle will use keys "w" for up and "s" for down
-// set up boundaries for left paddle
-const moveLeftPaddle = (e) => {
-    switch (e.keyCode) {
-        // left paddle board goes up
-        case (87):
+// set up canvas boundaries for left paddle
+    const moveLeftPaddle = (e) => {
+        switch (e.keyCode) {
+            // left paddle board goes up
+            case (87):
             leftPaddle.y -= 10
             if (leftPaddle.y <= 0) {
                 leftPaddle.y = 0
@@ -58,17 +43,64 @@ const moveLeftPaddle = (e) => {
             case (83):
             leftPaddle.y += 10
             if (leftPaddle.y + leftPaddle.height >= game.height) {
-                leftPaddle.y = game.height + leftPaddle.height
+                leftPaddle.y = game.height - leftPaddle.height
             }
             break
+                }
             }
-        }
+// set up keycode functionality for right paddle
+// right paddle will use keys arrow up and arrow down
+// set up canvas boundaries for right paddle
+    const moveRightPaddle = (e) => {
+        switch (e.keyCode) {
+            // right paddle board goes up
+            case (38):
+            rightPaddle.y -= 10
+            if (rightPaddle.y <= 0) {
+                rightPaddle.y = 0
+            }
+            break
+            // right paddle board goes down
+            case (40):
+            rightPaddle.y += 10
+            if (rightPaddle.y + rightPaddle.height >= game.height) {
+                rightPaddle.y = game.height - rightPaddle.height
+            }
+            break
+                }
+            }
+//constructor function for circle
+function Circle(x, y, r) {
+    this.x = x
+    this.y = y
+    this.r = r
+    this.draw = () => {
+        ctx.beginPath()
+        ctx.arc(canvas.width / 2, canvas.height / 2, r, 0, Math.PI*2, false)
+        ctx.fill()  
+    }
+}
+//render ball in the middle of canvas
+let ball = new Circle (100, 100, 20)
+console.log("this is the ball", ball)
+
+
+// set up gameLoop function
+const gameLoop = () => {
+    // clear the canvas
+    ctx.clearRect(0, 0, game.width, game.height)
+    // render the paddles
+    leftPaddle.render ()
+    rightPaddle.render ()
+    ball.draw ()
+}
 
 // declare a function that will stop our animation loop
-let stopGameLoop = () => {clearInterval(gameInterval)}
-
-// add event listener for left paddle
+// let stopGameLoop = () => {clearInterval(gameInterval)}
+                                
+// add event listeners for paddles
 document.addEventListener("keydown", moveLeftPaddle)
-
+document.addEventListener("keydown", moveRightPaddle)
+                                
 // declare a timing function will determine how and when the game animates
 let gameInterval = setInterval(gameLoop, 100)
