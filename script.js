3967a87fb0e6be1600f17908ab4e3a082ec9abbf
galyverasi <galyverasi@gmail.com>
2021-10-26 15:09:38 -0600

    detect collision on paddles

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ const game = document.getElementById("canvas")
 const ctx = game.getContext("2d")
 
 // declare variable for the score
-let score = 0
-let score2 = 0
+let leftScore = 0
+let rightScore = 0
 
 // write a function that will keep score on the left
 function scoreText() {
     ctx.font = "30px Arial"
     ctx.fillStyle = "#FFFFFF"
-    ctx.fillText(score, canvas.height / 3, 30)
-    ctx.fillText(+score2, 525, 30)
+    ctx.fillText(leftScore, 175, 30)
+    ctx.fillText(rightScore, 525, 30)
 }
-console.log("this is score", scoreText)
+// console.log("this is score", scoreText)
 
 // declare a variable for the net
 const netWidth = 4
@@ -110,9 +110,9 @@ const ball = {
     y: canvas.height / 2,
     size: 20,
     dx: 4,
-    dy: 3
+    dy: 3,
 }
-console.log("this is the ball", ball)
+// console.log("this is the ball", ball)
 // function drawBall(x, y, size) {
 //     ctx.beginPath()
 //     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
@@ -121,9 +121,25 @@ console.log("this is the ball", ball)
 // }
 // let ball = new drawBall (canvas.width/2, canvas.height /2, 20)
 
+// declare a function that will detect collision on the left paddle
+function leftPaddleHit () {
+    if (ball.x - ball.size < leftPaddle.x &&
+         ball.y + ball.size > leftPaddle.y &&
+         leftPaddle.x - leftPaddle.width < ball.x && 
+         leftPaddle.y + leftPaddle.height > ball.y) {
+        console.log("touch left")
+    }
+}
 
-
-
+// declare a function that will detect collission on the right paddle
+function rightPaddleHit () {
+    if (ball.x + ball.size > rightPaddle.x &&
+         ball.y + ball.size > rightPaddle.y &&
+         rightPaddle.x + rightPaddle.width > ball.x && 
+         rightPaddle.y + rightPaddle.height > ball.y) {
+        console.log("touch right")
+    }
+}
 
 // create a function that will animate the ball
 function gameLoop() {
@@ -138,115 +154,35 @@ function gameLoop() {
     ball.x += ball.dx
     ball.y += ball.dy
     
+    rightPaddleHit()
+    leftPaddleHit()
+
     // set left and right boundaries
     if  (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
         ball.dx *= -1
-    } function hitRightWall() {
+    } function detectWall() {
         if  (ball.x + ball.size > canvas.width) {
             console.log("hit right side")
-            score++
+            leftScore++
         } else if (ball.x - ball.size < 0) {
             console.log("hit left side")
-            score2++
+            rightScore++
         }
     }
-    // targetLeft = [leftPaddle.y - 45, leftPaddle = 45]
-    // if (ball.x - ball.size === 20 && (ball.y - ball.size === targetLeft[0] || targetLeft[1] === ball.y + ball.size)) {
-    //     console.log("hit left paddle")
-    // }
-    if (ball.x - ball.size === 20 && (ball.y - ball.size > canvas.height / 2 - 45 || canvas.height / 2 + 45 < ball.y + ball.size)) {
-            console.log("hit left paddle")
-        }
 
-    // if (ball.x + ball.size == 680 && ball.y + ball.size == (canvas.height / 2 - 45)) {
-    //     console.log("hit right paddle")
-    // }
-
-
-
-
-    // collide with right side of canvas
-    // if (ball.x + ball.size > rightPaddle.width) {
-    //     console.log("hit right paddle")
-    // }
-    // collide left side of canvas
-    // if (ball.x - ball.size < 0) {
-    //     console.log("left side of canvas")
-    // }
-    // else if (ball.x + ball.size < canvas.height - ball.size) {
-    //     if(ball.y >leftPaddle && ball.x < leftPaddle + paddleWidth) {
-    //         ball.dy = -ball.dy
-    //     }
-    //     else {
-    //         alert("game over")
-    //     }ß
-    // }
     // set top and bottom boundaries
     if  (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
         ball.dy *= -1
     }
 
-    // if (ball.x == 5) {
-    //     console.log("hit")
-    // }
-    
-    //or 680 then do this but only if the paddle is there
-
-
-
-
-
-    // if (leftPaddle.y + leftPaddle.width >= ball.size) {
-    //     console.log("hit")
-    // }
-    // if (ball.size + canvas.width >= canvas.width) {
-    // console.log("hello")
-    // }
-    // if (detectHit(ball, leftPaddle)) {
-    //     ball.dx *= -1
-    //     ball.x = leftPaddle.x + leftPaddle.width
-    // } else if (detectHit(ball, rightPaddle)) {
-    //     ball.dx *= -1
-    //     ball.x = rightPaddle.x - ball.width
-    // }
     requestAnimationFrame(gameLoop)
     // render the paddles
     leftPaddle.render()
     rightPaddle.render()
     scoreText()
-    hitRightWall()
+    detectWall()
 }
 gameLoop()
-
-//declare a function that will check for collision between paddle and ball
-function detectHit(paddle, ball) {
-    return  paddle.x < ball.x + ball.width &&
-            paddle.x + paddle.width > ball.x &&
-            paddle.y < ball.y + ball.height &&
-            paddle.y + paddle.height > ball.y
-}
-console.log(detectHit)
-
-// if (detectHit(ball, leftPaddle)) {
-//     ball.dx *= -1
-//     ball.x = leftPaddle.x + leftPaddle.width
-// }
-// else if (detectHit(ball, rightPaddle)) {
-//     ball.dx *= -1
-//     ball.x = rightPaddle.x - ball.width
-// }
-
-
-// set up gameLoop function
-
-// const gameLoop = () => {
-//     requestAnimationFrame(animateBall)
-//     // clear the canvas
-//     ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-//     // speed of paddles
-// }
-// gameLoop()
 
 // declare a function that will stop our animation loop
 // let stopGameLoop = () => {clearInterval(gameInterval)}
